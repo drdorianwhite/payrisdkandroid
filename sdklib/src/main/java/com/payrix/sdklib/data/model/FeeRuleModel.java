f60c2ffd3a9570c3e5471f70c9973013b4223873
Lehel Kovach <drdorianwhite@gmail.com>
2018-05-21 07:36:21 -0700

    models added

---
+++ sdklib/src/main/java/com/payrix/sdklib/data/model/FeeRuleModel.java
@@ -0,0 +1,12 @@
+package com.payrix.sdklib.data.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class FeeRuleModel  extends PayrixEntityModel {
+
+
+    @SerializedName("")
+    @Expose
+    private String login;
+}
